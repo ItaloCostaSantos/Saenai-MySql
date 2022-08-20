f40bae0a1d8c56bb8dfdb60a7014b4c256956faa
ItaloCostaSantos <italosantoscosta07@gmail.com>
2022-08-19 21:47:12 -0300

    tabela aluno backup

---
+++ Scripts/dump-wwonline05-202208192134.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: mysql.wwonline.com.br    Database: wwonline05
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.2.36-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aluno` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula` varchar(10) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `nome_mae` varchar(150) NOT NULL,
+  `nome_pai` varchar(150) DEFAULT NULL,
+  `endereco_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+INSERT INTO `aluno` VALUES (1,'10','José Costa de Jesus','176.228.454-81','(27)99715-3267','josedasilva17@gmail.com','1989-05-07','Maria da Costa','Sebastião de Jesus',1),(2,'11','Julia dos Santos Carvalho','198.547.213-13','(27)99214-1478','juliasantos13@gmail.com','1995-06-21','Bethania dos Santos','Junior Carvalho',2);
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `endereco` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `logradouro` varchar(150) NOT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `cep` varchar(10) NOT NULL,
+  `uf_id` int(11) NOT NULL COMMENT 'Chave estrangeira do ID da tabela UF',
+  `municipio_id` int(11) NOT NULL COMMENT 'Chave estrangeira ID da Tabela Municipio',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `endereco`
+--
+
+LOCK TABLES `endereco` WRITE;
+/*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
+INSERT INTO `endereco` VALUES (1,'Av. Filogônio Peixoto','245','Aviso','29901290',63,1),(2,'R. Pres. Castelo Branco','327','Goiabeiras','29075053',63,2);
+/*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `filho`
+--
+
+DROP TABLE IF EXISTS `filho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filho` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filho`
+--
+
+LOCK TABLES `filho` WRITE;
+/*!40000 ALTER TABLE `filho` DISABLE KEYS */;
+/*!40000 ALTER TABLE `filho` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `municipio`
+--
+
+DROP TABLE IF EXISTS `municipio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `municipio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `municipio`
+--
+
+LOCK TABLES `municipio` WRITE;
+/*!40000 ALTER TABLE `municipio` DISABLE KEYS */;
+INSERT INTO `municipio` VALUES (1,'LINHARES'),(2,'VITÓRIA'),(3,'SÃO MATEUS'),(4,'SOORETAMA'),(5,'COLATINA'),(6,'ARACRUZ'),(7,'SERRA'),(8,'GUARAPARI'),(9,'JAGUARÉ'),(10,'RIO BANANAL'),(11,'LINHARES'),(12,'VITÓRIA'),(13,'SÃO MATEUS'),(14,'SOORETAMA'),(15,'COLATINA'),(16,'ARACRUZ'),(17,'SERRA'),(18,'GUARAPARI'),(19,'JAGUARÉ'),(20,'RIO BANANAL');
+/*!40000 ALTER TABLE `municipio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uf`
+--
+
+DROP TABLE IF EXISTS `uf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uf` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sigla` varchar(2) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `codigo_ibge` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uf`
+--
+
+LOCK TABLES `uf` WRITE;
+/*!40000 ALTER TABLE `uf` DISABLE KEYS */;
+INSERT INTO `uf` VALUES (56,'AC','ACRE',12),(57,'AL','ALAGOAS',27),(58,'AP','AMAPÁ',16),(59,'AM','AMAZONAS',13),(60,'BA','BAHIA',29),(61,'CE','CEARÁ',23),(62,'DF','DISTRITO FEDERAL',53),(63,'ES','ESPÍRITO SANTO',32),(64,'GO','GOIÁS',52),(65,'MA','MARANHÃO',21),(66,'MT','MATO GROSSO',51),(67,'MS','MATO GROSSO DO SUL',50),(68,'MG','MINAS GERAIS',31),(69,'PA','PARÁ',15),(70,'PB','PARAÍBA',25),(71,'PR','PARANÁ',41),(72,'PE','PERNAMBUCO',26),(73,'PI','PIAUÍ',22),(74,'RN','RIO GRANDE DO NORTE',24),(75,'RS','RIO GRANDE DO SUL',43),(76,'RJ','RIO DE JANEIRO',33),(77,'RO','RONDÔNIA',11),(78,'RR','RORAIMA',14),(79,'SC','SANTA CATARINA',42),(80,'SP','SÃO PAULO',35),(81,'SE','SERGIPE',28),(82,'TO','TOCANTINS',17);
+/*!40000 ALTER TABLE `uf` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'wwonline05'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-19 21:34:13
